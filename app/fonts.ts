c05607dd633fbcf517dd476c6dd26ce1e4765392
Prateek <pratikhakay@gmail.com>
2025-03-10 17:21:48 +0100

    Fixed the font issue for Vercel Deployment

--- app/fonts.ts
+++ app/fonts.ts
@@ -3,32 +3,32 @@ import localFont from 'next/font/local';
 export const aeonik = localFont({
   src: [
     {
-      path: '../public/fonts/AeonikTRIAL-Light.otf',
+      path: './fonts/AeonikTRIAL-Light.otf',
       weight: '300',
       style: 'normal',
     },
     {
-      path: '../public/fonts/AeonikTRIAL-LightItalic.otf',
+      path: './fonts/AeonikTRIAL-LightItalic.otf',
       weight: '300',
       style: 'italic',
     },
     {
-      path: '../public/fonts/AeonikTRIAL-Regular.otf',
+      path: './fonts/AeonikTRIAL-Regular.otf',
       weight: '400',
       style: 'normal',
     },
     {
-      path: '../public/fonts/AeonikTRIAL-RegularItalic.otf',
+      path: './fonts/AeonikTRIAL-RegularItalic.otf',
       weight: '400',
       style: 'italic',
     },
     {
-      path: '../public/fonts/AeonikTRIAL-Bold.otf',
+      path: './fonts/AeonikTRIAL-Bold.otf',
       weight: '700',
       style: 'normal',
     },
     {
-      path: '../public/fonts/AeonikTRIAL-BoldItalic.otf',
+      path: './fonts/AeonikTRIAL-BoldItalic.otf',
       weight: '700',
       style: 'italic',
     },
